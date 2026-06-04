08ad738898274c9bde1ae82a574abca210498636
Petro <petropavliuk12.05@gmail.com>
2026-06-05 00:42:44 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -20,4 +20,41 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
     """
     # TODO: Implement this function
     # Write your tests first, then implement the logic
-    return [0, 0]
+    result = [0, 0]
+    if cat_age <= 14 and dog_age <= 14:
+        return result
+
+    if cat_age >= 15:
+        cat_age -= 15
+        result[0] += 1
+
+    if dog_age >= 15:
+        dog_age -= 15
+        result[1] += 1
+
+    if cat_age < 9 and dog_age < 9:
+        return result
+
+    if cat_age >= 9:
+        cat_age -= 9
+        result[0] += 1
+
+    if dog_age >= 9:
+        dog_age -= 9
+        result[1] += 1
+
+    while True:
+        if cat_age >= 4:
+            cat_age -= 4
+            result[0] += 1
+        if cat_age < 4:
+            break
+
+    while True:
+        if dog_age >= 5:
+            dog_age -= 5
+            result[1] += 1
+        if dog_age < 5:
+            break
+
+    return result
--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_years,dog_years,result",
+    [
+        pytest.param(0, 0, [0, 0]),
+        pytest.param(14, 14, [0, 0]),
+        pytest.param(15, 15, [1, 1]),
+        pytest.param(23, 23, [1, 1]),
+        pytest.param(24, 24, [2, 2]),
+        pytest.param(27, 27, [2, 2]),
+        pytest.param(28, 28, [3, 2]),
+        pytest.param(100, 100, [21, 17])
+    ]
+)
+def test_different_years_correctness(
+    cat_years: int, dog_years: int, result: list
+) -> None:
+    assert get_human_age(cat_years, dog_years) == result
